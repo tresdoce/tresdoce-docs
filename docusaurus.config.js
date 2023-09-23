26dcdad61e244c8caf52d793b5a7269b464ace91
Maximiliano Delgado <mdelgado@tresdoce.com.ar>
2023-09-23 17:22:14 -0300

    chore: update documentation

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,9 +6,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'CodePedia',
+  title: 'TresDoce - Docs',
   tagline: 'Acá vas a encontrar recursos para empezar a desarrollar tus aplicaciones con NestJS.',
-  url: 'https://tresdoce-docs.vercel.app',
+  url: 'https://docs.tresdoce.com.ar',
   baseUrl: '/',
   favicon: 'img/favicon.ico',
   organizationName: 'tresdoce', // Usually your GitHub org/user name.
@@ -25,7 +25,7 @@ const config = {
       //image: '/img/supabase-og-image.png', // used for meta tag, in particular og:image and twitter:image
       //metaImage: '/img/supabase-og-image.png',
       navbar: {
-        title: 'CodePedia',
+        title: 'TresDoce - Docs',
         hideOnScroll: false,
         logo: {
           alt: 'NestJSPedia Logo',
@@ -33,18 +33,18 @@ const config = {
           //src: 'https://camo.githubusercontent.com/c704e8013883cc3a04c7657e656fe30be5b188145d759a6aaff441658c5ffae0/68747470733a2f2f6e6573746a732e636f6d2f696d672f6c6f676f5f746578742e737667',
         },
         items: [
-          /*{
+          {
             type: 'doc',
             docId: 'intro',
             position: 'left',
             label: 'Tutorial',
-          },*/
-          /*{ to: '/blog', label: 'Blog', position: 'left' },*/
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
           /*{
             type: 'localeDropdown',
             position: 'right',
-          },*/
-          /*{
+          },
+          {
             type: 'docsVersionDropdown',
             position: 'right',
           },*/
@@ -124,12 +124,12 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           //editUrl: 'https://github.com/tresdoce/tresdoce-docs/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        /*blog: {
+        blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           //editUrl: 'https://github.com/tresdoce/tresdoce-docs/tree/main/packages/create-docusaurus/templates/shared/',
-        },*/
+        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
